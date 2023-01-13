106554af14734c00488d784874fd7da47313c095
Sebastian <sebyzy@gmail.com>
2023-01-13 14:18:39 -0500

    1.13

--- AgingReportGenerator_PW.py
+++ AgingReportGenerator_PW.py
@@ -8,3 +8,12 @@ class Customers:
     credit_limit = ""
     salesperson = ""
     isObsoleted = ""
+     # The init method or constructor
+    def __init__(self, title):
+        self.title = title
+    # setter method to set customer name
+    def set_customer_name(self, name):
+        self.name = name    
+
+    def get_customer_name(self):
+        return self.name
