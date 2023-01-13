309fe05d3bb882defcf7ab5b2f1c739734b9697e
Sebastian <sebyzy@gmail.com>
2023-01-13 11:29:46 -0500

    added customer class

---
+++ AgingReportGenerator_PW.py
@@ -0,0 +1,10 @@
+
+
+class Customers:
+    customer_name = ""
+    customer_code = ""
+    customer_type = ""
+    payment_term = ""
+    credit_limit = ""
+    salesperson = ""
+    isObsoleted = ""
