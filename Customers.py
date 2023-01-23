fe8e66ddb7a308fa5c76c2ad40438991785ca03d
seb <sebyzy@gmail.com>
2023-01-22 22:01:54 -0500

    1.22

---
+++ Customers.py
@@ -0,0 +1,61 @@
+class Customers:
+    # The init method or constructor
+
+    def __init__(self, title):
+        self.title = title
+    # setter method for customer name
+
+    def set_customer_name(self, name):
+        self.name = name
+
+    # getter method for customer name
+    def get_customer_name(self):
+        return self.name
+
+    # setter method for customer code
+    def set_customer_code(self, name):
+        self.name = name
+    # getter method for customer code
+
+    def get_customer_code(self):
+        return self.name
+    # setter method for customer type
+
+    def set_customer_type(self, name):
+        self.name = name
+    # getter method for customer type
+
+    def get_customer_type(self):
+        return self.name
+    # setter method for customer's payment term
+
+    def set_customer_payment_term(self, name):
+        self.name = name
+    # getter method for customer's payment term
+
+    def get_customer_payment_term(self):
+        return self.name
+    # setter method for customer's credit limit
+
+    def set_customer_credit_limit(self, name):
+        self.name = name
+    # getter method for customer's credit limit
+
+    def get_customer_credit_limit(self):
+        return self.name
+    # setter method for customer's associated salesperson
+
+    def set_customer_salesperson(self, name):
+        self.name = name
+    # getter method for customer's associated salesperson
+
+    def get_customer_salesperson(self):
+        return self.name
+    # setter method for customer's obsoleted status
+
+    def set_customer_isObsoleted_status(self, name):
+        self.name = name
+    # getter method for customer's obsoleted status
+
+    def get_customer_isObsoleted_status(self):
+        return self.name
