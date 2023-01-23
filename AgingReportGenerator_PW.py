e92d7b14507c7f6a7034c61832c9e7164abc06c9
seb <sebyzy@gmail.com>
2023-01-22 22:01:47 -0500

    1.22

--- AgingReportGenerator_PW.py
+++ AgingReportGenerator_PW.py
@@ -1,19 +1,53 @@
+import csv
+import json
+import Customers
 
 
-class Customers:
-    customer_name = ""
-    customer_code = ""
-    customer_type = ""
-    payment_term = ""
-    credit_limit = ""
-    salesperson = ""
-    isObsoleted = ""
-    # The init method or constructor
-    def __init__(self, title):
-        self.title = title
-    # setter method to set customer name
-    def set_customer_name(self, name):
-        self.name = name    
 
-    def get_customer_name(self):
-        return self.name
+# Function to convert a CSV to JSON
+# Takes the file paths as arguments
+def make_json(csvFilePath, jsonFilePath, primaryKey):
+
+    # create a dictionary
+    data = {}
+
+    # Open a csv reader called DictReader
+    with open(csvFilePath, encoding='utf-8') as csvf:
+        csvReader = csv.DictReader(csvf)
+
+        # Convert each row into a dictionary
+        # and add it to data
+        for rows in csvReader:
+
+            # Assuming a column named 'No' to
+            # be the primary key
+            key = rows[str(primaryKey)]
+            data[key] = rows
+
+    # Open a json writer, and use the json.dumps()
+    # function to dump data
+    with open(jsonFilePath, 'w', encoding='utf-8') as jsonf:
+        jsonf.write(json.dumps(data, indent=4))
+
+
+def main():
+    customer_name = "a1"
+    customer_code = "a010"
+    customer_type = "food"
+    payment_term = "cod"
+    credit_limit = "1000"
+    salesperson = "03:avin"
+    isObsoleted = "false"
+    csvFilePath = r'Customers.csv'
+    jsonFilePath = r'Customers.json'
+
+    # Call the make_json function
+    make_json(csvFilePath, jsonFilePath, "*Customer Name")
+    customer = Customers.Customers(customer_name)
+    customer.set_customer_name(customer_name)
+    customer.set_customer_code(customer_code)
+    
+    print(customer.get_customer_code())
+
+if __name__ == "__main__":
+    main()
